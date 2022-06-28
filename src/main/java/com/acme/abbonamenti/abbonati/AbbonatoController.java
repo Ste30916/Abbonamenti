3ba032f6e0ca6404ae3d32c426a1f3c565a78a4d
rosam <rosam@LAPTOP-J9NR809C.mshome.net>
2022-06-28 16:53:44 +0200

    commit 5 rosa

--- src/main/java/com/acme/abbonamenti/abbonati/AbbonatoController.java
+++ src/main/java/com/acme/abbonamenti/abbonati/AbbonatoController.java
@@ -1,5 +1,37 @@
 package com.acme.abbonamenti.abbonati;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/abbonato")
 public class AbbonatoController {
+	@Autowired
+	AbbonatoRepository abbonatoRepo;
+	@Autowired
+	AbbonatoService abbonatoService;
+	
+	@GetMapping
+	public ResponseEntity<?> getAllAbbonati(){
+		return ResponseEntity.ok(abbonatoRepo.findAll());
+		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getAbbonato(@PathVariable long id){
+		return ResponseEntity.ok(abbonatoRepo.findById(id));
  
+}
+	@PostMapping
+	public ResponseEntity<?> inserisciAbbonato(@RequestBody AbbonatoDto dto) throws AlreadyInsertedException{
+		abbonatoService.inserisciAbbonato(dto);
+		return ResponseEntity.ok("Abbonato inserito");
+	}
+	
 }
