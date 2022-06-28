586edbd9dd21decfa987a453e5b4b908305114fd
Ste30916 <ste30916@gmail.com>
2022-06-28 15:32:07 +0200

    Commit

---
+++ src/main/java/com/acme/abbonamenti/AbbonamentiApplication.java
@@ -0,0 +1,13 @@
+package com.acme.abbonamenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbbonamentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbbonamentiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/abbonamenti/AbbonamentiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.abbonamenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbbonamentiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
