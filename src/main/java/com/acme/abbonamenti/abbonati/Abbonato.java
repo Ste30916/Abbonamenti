ebf1068a8538878bf84ee2d65654c98afdb3f319
rosam <rosam@LAPTOP-J9NR809C.mshome.net>
2022-06-28 15:55:42 +0200

    commit rosa 1

---
+++ src/main/java/com/acme/abbonamenti/abbonati/Abbonato.java
@@ -0,0 +1,41 @@
+package com.acme.abbonamenti.abbonati;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.acme.abbonamenti.abbonamenti.Abbonamento;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "abbonati")
+public class Abbonato {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String nome;
+	
+	private String cognome;
+	
+	private String codiceFiscale;
+	
+	@ToString.Exclude
+	@JsonIgnoreProperties({"abbonato"})
+	@OneToMany(mappedBy = "abbonato")
+	private List<Abbonamento> abbonamenti;
+
+}
