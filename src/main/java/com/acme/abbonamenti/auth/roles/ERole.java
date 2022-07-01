7949d8f22d117c70d5effc9d0fcbd16115e9bc2e
rosam <rosam@LAPTOP-J9NR809C.mshome.net>
2022-07-01 15:38:50 +0200

    commit security

---
+++ src/main/java/com/acme/abbonamenti/auth/roles/ERole.java
@@ -0,0 +1,6 @@
+package com.acme.abbonamenti.auth.roles;
+
+public enum ERole {
+	ROLE_ADMIN,
+	ROLE_USER;
+}
---
+++ src/main/java/com/acme/abbonamenti/auth/roles/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.acme.abbonamenti.auth.roles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+
+
+}
